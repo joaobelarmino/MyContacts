6e676b8e65c599ea6e059deb6c49e672cc6836f5
João Belarmino <joao.belarmino.silva01@gmail.com>
2023-01-30 12:32:04 -0300

    feat: contact card created

--- fe/src/components/ContactsList/style.js
+++ fe/src/components/ContactsList/style.js
@@ -23,3 +23,87 @@ export const Header = styled.div`
     border-radius: 4px;
   }
 `;
+
+export const ListContainer = styled.div`
+  margin-top: 16px;
+  padding-top: 16px;
+  border-top: 2px solid ${({ theme }) => theme.colors.gray.normal};
+  border-radius: 2px;
+
+  header {
+    margin-bottom: 8px;
+
+    .sort-button {
+      display: flex;
+      align-items: center;
+      background-color: transparent;
+      border: none;
+      outline: none;
+
+      span {
+        color: ${({ theme }) => theme.colors.primary.main};
+        margin-right: 8px;
+        font-weight: bold;
+        font-size: 16px;
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .contact {
+    &__heading {
+      display: flex;
+      align-items: center;
+
+      strong {
+        font-size: 16px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.gray.darker};
+        margin-right: 8px;
+      }
+      small {
+        font-size: 12px;
+        font-weight: bold;
+        text-transform: uppercase;
+        color: ${({ theme }) => theme.colors.primary.main};
+        background-color: ${({ theme }) => theme.colors.primary.lighter};
+        padding: 4px 8px;
+        border-radius: 4px
+      }
+    }
+
+    &__info {
+      display: flex;
+      flex-direction: column;
+      margin-top: 4px;
+      color: ${({ theme }) => theme.colors.gray.normal}
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    button {
+      background-color: transparent;
+      border: none;
+      outline: none;
+      position: relative;
+      top: -1px;
+    }
+  }
+`;
