1f758821706e6185815b237845a34151bf651405
João Belarmino <joao.belarmino.silva01@gmail.com>
2023-02-01 18:27:01 -0300

    refactor: refactoring ContactForm error handling
    Creating customHook to management of error states on input fields.

---
+++ fe/src/hooks/useErrors.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function useErrors() {
+  const [errors, setErrors] = useState([]);
+
+  function setError({ field, message }) {
+    const errorAlreadyExists = errors.find((error) => error.message === message);
+    if (errorAlreadyExists) { return; }
+
+    setErrors((prevState) => [
+      ...prevState,
+      { field, message },
+    ]);
+  }
+
+  function removeError(message) {
+    setErrors((prevState) => prevState.filter(
+      (error) => error.message !== message,
+    ));
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  return { setError, removeError, getErrorMessageByFieldName };
+}
